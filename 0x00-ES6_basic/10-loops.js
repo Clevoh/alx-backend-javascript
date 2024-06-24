45889b32f8c73b04d3914fe44c1ddef5e465cc3a
Clevoh <clevelihasi@gmail.com>
2024-06-24 15:33:34 +0000

    10. For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+import default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
