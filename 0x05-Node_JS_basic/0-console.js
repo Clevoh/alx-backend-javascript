3b13f973eaaeceee3db13976e90db1ec135f052d
Clevoh <clevelihasi@gmail.com>
2024-08-21 03:57:01 +0000

    0. Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * dispaly a message to the STDOUT
+ * @param {String} message the message to be display
+ * @author Cleve Lihasi <https://github.com/Clevoh>
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
